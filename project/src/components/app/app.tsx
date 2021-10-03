bb804419d6030f0bf4e4a7001f9f1193247398e8
orlov valentin <orlovvalentino@gmail.com>
2021-10-04 00:00:51 +0300

    create components for each page

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,17 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import HomePage from '../home-page/home-page';
+
+type MoviePromo = {
+  moviePromo: {
+    promoName: string,
+    promoGenre: string,
+    promoDate: string
+  }
+}
+
+function App({moviePromo}: MoviePromo): JSX.Element {
+  return (
+    <HomePage moviePromo={moviePromo} />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const moviePromo = {
+  promoName: 'The Grand Budapest Hotel',
+  promoGenre: 'dram',
+  promoDate: '2014',
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App moviePromo={moviePromo}/>
   </React.StrictMode>,
   document.getElementById('root'));
