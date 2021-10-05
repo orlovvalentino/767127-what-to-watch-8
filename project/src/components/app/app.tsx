13f2b12ff41e20864589645ba65740f40d8127bd
orlov valentin <orlovvalentino@gmail.com>
2021-10-05 23:23:45 +0300

    3 - add routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,10 @@
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import HomePage from '../home-page/home-page';
+import SignIn from '../sign-in/sign-in';
+import MyList from '../my-list/my-list';
+import FilmPage from '../film-page/film-page';
+import AddReview from '../add-review/add-review';
+import Player from '../player/player';
 
 type MoviePromo = {
   moviePromo: {
@@ -11,7 +16,28 @@ type MoviePromo = {
 
 function App({moviePromo}: MoviePromo): JSX.Element {
   return (
-    <HomePage moviePromo={moviePromo} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <HomePage moviePromo={moviePromo} />
+        </Route>
+        <Route exact path="/login">
+          <SignIn/>
+        </Route>
+        <Route exact path="/mylist">
+          <MyList/>
+        </Route>
+        <Route exact path="/films/:id">
+          <FilmPage/>
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReview/>
+        </Route>
+        <Route exact path="/player/:id">
+          <Player/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
