186c75934a2f2898fe75fdf7610119e1620b7b01
orlov valentin <orlovvalentino@gmail.com>
2021-10-31 23:50:03 +0300

    Задание 6.1.3

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import {films} from './mocks/films';
+import {reducer} from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App films = {films}/>
+    <Provider store = {store}>
+      <App films = {films}/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
