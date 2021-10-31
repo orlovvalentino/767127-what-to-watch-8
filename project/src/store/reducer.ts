b4990a2b4a84a8f49e97c42dcd44e7014a5f5a45
orlov valentin <orlovvalentino@gmail.com>
2021-10-31 21:46:48 +0300

    Задание 6.1.2

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,13 +1,18 @@
 import {films} from '../mocks/films';
 import {State} from '../types/state';
+import {Actions, ActionType} from '../types/action';
 
 const initialState = {
   genre: 'triller',
-  films: films
+  films: films,
 };
 
-const reducer = (state: State = initialState, action: any): State => {
+const reducer = (state: State = initialState, action: Actions): State => {
   switch (action.type) {
+    case ActionType.ChangeGenre:
+      return {...state, genre: action.payload};
+    case ActionType.GetListFilms:
+      return {...state, films: films};
     default:
       return state;
   }
