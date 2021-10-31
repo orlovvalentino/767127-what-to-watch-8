63a0520b8729fb0e02f9aa464880c57d30577e2f
orlov valentin <orlovvalentino@gmail.com>
2021-10-31 20:20:39 +0300

    добавления активного класса для табов

--- project/src/components/film-page-tabs/film-page-tabs.tsx
+++ project/src/components/film-page-tabs/film-page-tabs.tsx
@@ -1,5 +1,5 @@
 import {Film} from '../../types/films';
-import {NavLink, Route, useRouteMatch} from 'react-router-dom';
+import {Link, Route, useRouteMatch} from 'react-router-dom';
 import FilmPageOverview from '../film-page-overview/film-page-overview';
 import FilmPageDetails from '../film-page-details/film-page-details';
 import FilmPageReview from '../film-page-review/film-page-review';
@@ -10,39 +10,38 @@ type PropsType = {
 
 function FilmPageTabs({film}: PropsType): JSX.Element {
   const {path, url } = useRouteMatch();
+
+  function GetActiveClass (urlParam:string): string{
+    return (useRouteMatch({path:`${path}/${urlParam}`,exact: true}) ? 'film-nav__item--active' : '');
+  }
+
   return (
     <div className="film-card__desc">
       <nav className="film-nav film-card__nav">
         <ul className="film-nav__list">
-          <li className="film-nav__item">
-            <NavLink
-              exact
+          <li className={`film-nav__item ${GetActiveClass('')}`}>
+            <Link
               to={url}
               className="film-nav__item film-nav__link"
-              activeClassName="film-nav__item--active"
             >
               Overview
-            </NavLink>
+            </Link>
           </li>
-          <li className="film-nav__item">
-            <NavLink
-              exact
+          <li className={`film-nav__item ${GetActiveClass('details')}`}>
+            <Link
               to={`${url}/details`}
               className="film-nav__item film-nav__link"
-              activeClassName="film-nav__item--active"
             >
               Details
-            </NavLink>
+            </Link>
           </li>
-          <li className="film-nav__item">
-            <NavLink
-              exact
+          <li className={`film-nav__item ${GetActiveClass('reviews')}`}>
+            <Link
               to={`${url}/reviews`}
               className="film-nav__item film-nav__link"
-              activeClassName="film-nav__item--active"
             >
               Reviews
-            </NavLink>
+            </Link>
           </li>
         </ul>
       </nav>
