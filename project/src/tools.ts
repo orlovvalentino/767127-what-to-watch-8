b06db0c6a998ebe169f2599773d55e57ac408896
orlov valentin <orlovvalentino@gmail.com>
2021-11-02 20:00:46 +0300

    Задание 6.1.4

--- project/src/tools.ts
+++ project/src/tools.ts
@@ -1,5 +1,12 @@
 import {Film, Films} from './types/films';
 
-export function getCurrentFilm(films: Films, id?: string): Film{
+export function getCurrentFilm(films: Films, id?: string): Film {
   return films.find((item: Film) => item.id.toString() === id) ?? films[0];
+}
+
+export function getFilmsByGenre(films: Films, genre: string): Films {
+  if(genre.toLowerCase() === 'all genres'){
+    return films;
+  }
+  return films.filter((item: Film) => item.genre.toLowerCase() === genre.toLowerCase()) ?? films
 }
