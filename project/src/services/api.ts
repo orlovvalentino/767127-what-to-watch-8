2613580b1317ddcfe344e1bdb7531573a5a35816
orlov valentin <orlovvalentino@gmail.com>
2021-11-14 21:04:35 +0300

    module7-task1: 2.Создайте новый модуль api.ts

---
+++ project/src/services/api.ts
@@ -0,0 +1,29 @@
+import axios, {AxiosInstance, AxiosResponse, AxiosError} from 'axios';
+
+const BACKEND_URL = 'https://8.react.pages.academy/wtw';
+const REQUEST_TIMEOUT = 5000;
+
+type UnauthorizedCallback = () => void;
+
+export const createAPI = (onUnauthorized: UnauthorizedCallback): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  api.interceptors.response.use(
+    (response: AxiosResponse) => response,
+
+    (error: AxiosError) => {
+      const {response} = error;
+
+      if (response?.status === HttpCode.Unauthorized) {
+        return onUnauthorized();
+      }
+
+      return Promise.reject(error);
+    },
+  );
+
+  return api;
+};
