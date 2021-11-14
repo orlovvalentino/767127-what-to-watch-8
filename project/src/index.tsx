9c24b6cf8e8cb48aa801c38a0d77e1c57e74e379
orlov valentin <orlovvalentino@gmail.com>
2021-11-14 21:12:27 +0300

    module7-task1: 3.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import thunk from 'redux-thunk';
+import {createAPI} from './services/api';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
