dd4207ca9a618d1303a0d52f6ff056bb8b1e6596
orlov valentin <orlovvalentino@gmail.com>
2021-11-19 13:59:01 +0300

    module7-task2: правки от наставника

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -1,17 +1,10 @@
 import {ThunkActionResult} from '../types/action';
 import {AuthData} from '../types/auth-data';
 import {saveToken, Token} from '../services/token';
-import {
-  getListFilms,
-  setAuthorizationStatus,
-  setFilteredFilms,
-  redirectToRoute,
-  setCurrentFilm,
-  setSimilarFilms, setComments
-} from './action';
+import {getListFilms, setAuthorizationStatus, setFilteredFilms,redirectToRoute} from './action';
 import {APIRoute,AppRoute} from '../const';
 import {ServerFilms} from '../types/serverFilms';
-import {adaptFilmsToClient,adaptFilmToClient} from '../services/adapter';
+import {adaptFilmsToClient} from '../services/adapter';
 
 
 export const fetchFilmsAction = (): ThunkActionResult =>
@@ -35,20 +28,4 @@ export const loginAction = ({login: email, password}: AuthData): ThunkActionResu
     dispatch(redirectToRoute(AppRoute.Root));
   };
 
-export const getCurrentFilm = (id:string | undefined):ThunkActionResult=>
-  async (dispatch, _getState, api): Promise<void> => {
-    const {data} = await api.get(`${APIRoute.Films}/${id}`);
-    dispatch(setCurrentFilm(adaptFilmToClient(data)));
-  };
 
-export const getSimilarFilms = (id:string | undefined):ThunkActionResult=>
-  async (dispatch, _getState, api): Promise<void> => {
-    const {data} = await api.get(`${APIRoute.Films}/${id}/similar`);
-    dispatch(setSimilarFilms(adaptFilmsToClient(data)));
-  };
-
-export const getComments = (id:string | undefined):ThunkActionResult=>
-  async (dispatch, _getState, api): Promise<void> => {
-    const {data} = await api.get(`${APIRoute.Comments}/${id}`);
-    dispatch(setComments(data));
-  };
